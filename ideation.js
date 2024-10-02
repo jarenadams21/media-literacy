7bdd33e81dce1fbaf6e3348fa89d34eae7ac8629
jaren adams <jarenadams@jarens-Laptop.local>
2024-10-02 13:37:37 -0400

    start

---
+++ ideation.js
@@ -0,0 +1,6 @@
+// Running web archive that auto handles content updates
+
+// You'll own nothing, and be happy
+/// Anti modern SBM and SAAS initiatives -- per Ida Auken
+
+// Democratize, Find, Understand, Implement Forward
